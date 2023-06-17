317cf2a23f812d75c1506fcef9204e9e151c5bcc
Jerome Sloss <jeromesloss@yahoo.com>
2023-06-16 20:20:14 -0400

    message

--- app.js
+++ app.js
@@ -76,16 +76,17 @@ console.log(e == 'Kevin');
 //    driver = true;
 // }
 // console.log(driver);
-let age = 16 ;
-const person = "John Doe";
-function myFunction(person) {
-if (person >= 16) {
-   return("Here are the keys");
-} else {
-   return("Sorry you're too young");
- }
-}
-console.log(myFunction(person));
+
+// let age = 16 ;
+// const person = "John Doe";
+// function myFunction(person) {
+// if (person >= 16) {
+//    return("Here are the keys");
+// } else {
+//    return("Sorry you're too young");
+//  }
+// }
+// console.log(myFunction(person));
 
 
 
@@ -107,16 +108,72 @@ console.log(myFunction(person));
 
 // 3. Write a loop that will print out every third number starting with 12 and going no higher than 4000
 
-for (let i = 12; i <= 4000; i += 3) {
-   console.log(i);
-}
+// for (let i = 12; i <= 4000; i += 3) {
+//    console.log(i);
+
 //Get Even
+// Print out the numbers that are within the range of 1 - 100
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+// for (let i = 2; i <= 100; i += 2) {
+//    console.log(i + "<-- is an even number");
+// }
 
 //Give Me 5
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five:
+// for (let i = 1; i <= 100; i++) {
+//    if(i % 5==0 && i % 3==0) {
+//          console.log(i + " I found a number. High five!" + " I found a 3. Three is a crowd");
+//       }
+   
+
 
 //Savings Account
 
 //ARRAYS & CONTROL FLOW
+/* The things is arrays are called values.
+They always guarntee values will be in order. 
+You could model cars with an array*/
+
+// ### B. Easy Does It
+// Create an array that contains three quotes and store it in a variable called quotes
+// // 1.
+// const quotes = ["coding is great", "I am sleepy", "coding is a way out"];
+
+// ### C. Accessing elements
+
+// Given the following array `const randomThings = [1, 10, "Hello", true]`
+
+// - How do you access the 1st element in the array?
+// randomthings [0];
+// - Change the value of `"Hello"` to `"World"`
+// randomthings [2] = "World";
+// - Check the value of the array to make sure it updated the array using `console.log()`
+// console.log(randomthings);
+
+// ### D. Change values
+// Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// What would you write to access the 3rd element of the array?
+// ourClass [2];
+// // Change the value of "Github" to "Octocat"
+// ourClass [4] = "Octocat";
+// // Add a new element, "Cloud City" to the array
+// ourClass.push("Cloud City");
+
+// ### E. Mix It Up
+
+// Note: You don't really need `.splice()` for these. You could use it, but there are simpler array methods that are more appropriate.
+
+// Given the following array: `const myArray = [5, 10, 500, 20]`
+
+// - Add the string "Aegon"to the end of the array. Add another string of your choice to the end of the array.
+// - Remove the 5from the beginning of the array.
+// - Add the string "Bob Marley"to the beginning of the array.
+// - Remove the string of your choice from the end of the array.
+// - Reverse this array using `Array.prototype.reverse()`. 
+// - Did you mutate the array? What does mutate mean? Did the .reverse()method return anything?
+
+
 
 //OBJECTS
 
@@ -127,6 +184,4 @@ for (let i = 12; i <= 4000; i += 3) {
 //     }
 // );
 
-// console.log(longest)
-
-
+// console.log(longest);
